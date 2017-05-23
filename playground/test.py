44f6c5754a34a566b643bb7aeed22d35a8730030
Jagadeesh <jagadeesh.cheluvaraj@gmail.com>
2017-05-23 08:57:53 +0530

    Update from VS

---
+++ playground/test.py
@@ -0,0 +1,8 @@
+
+x = 1
+type_var = type(x)
+
+print(type_var)
+print(type(type_var))
+
+
---
+++ workaroud.py
@@ -0,0 +1,20 @@
+varA = 2
+varB = 4
+name = "name"
+
+if (type(varA) == str or type(varB) == str):
+    print("string involved")
+elif (type(varA) == int or type(varB) == int):
+    print("string did not invlved")
+elif varA > varB:
+    print("bigger")
+elif varA == varB:
+    print("equal")
+elif varA < varB:
+    print("smaller")
+
+
+try:
+    2/0
+except:
+    print("No divide by zero")
