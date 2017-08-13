b6f1cb4ff4d589d0fcd32a7884bcdb8f60f6b3ad
Jagadeesh <jagadeesh.cheluvaraj@gmail.com>
2017-08-13 07:04:32 +0530

    test update

--- REST_APIs_with_Flask_and_Python/section3/playground/app.py
+++ REST_APIs_with_Flask_and_Python/section3/playground/app.py
@@ -5,8 +5,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/') # 'http://www.googl.com/'
-def home():
-    return 'Hello, Universe :-)'
+
+# POST - used to recive data
+# GET - used to send data back only
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>', methods=['GET'])   # http://127.0.0.1/strore/some_name
+def get_store(name):
+    pass
+
+# GET /store
+# POST /store/<string:name>/item
+# GET /store/<string:name>/item
+
+
 
 app.run(port=5000)
