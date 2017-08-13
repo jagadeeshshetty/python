e79ca071019dffd69ecb514a16c35c2299cc3994
Jagadeesh <jagadeesh.cheluvaraj@gmail.com>
2017-08-13 08:18:49 +0530

    Update after 42. Returning a list of stores

--- REST_APIs_with_Flask_and_Python/section3/playground/app.py
+++ REST_APIs_with_Flask_and_Python/section3/playground/app.py
@@ -1,9 +1,21 @@
 #!/anaconda/bin/python
 """ It's a learing project."""
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
 
 
 # POST - used to recive data
@@ -14,15 +26,28 @@ app = Flask(__name__)
 def create_store():
     pass
 
+
 # GET /store/<string:name>
 @app.route('/store/<string:name>', methods=['GET'])   # http://127.0.0.1/strore/some_name
 def get_store(name):
     pass
 
+
 # GET /store
+@app.route('/store', methods=['GET'])
+def get_stores():
+    return jsonify({'stores': stores})
+
+
 # POST /store/<string:name>/item
+@app.route('/store/<string:name>/item', methods=['POST'])
+def creare_item_in_store(name):
+    pass
+
+
 # GET /store/<string:name>/item
-
-
+@app.route('/store/<string:name>/item', methods=['GET'])
+def get_items_in_store():
+    pass
 
 app.run(port=5000)
